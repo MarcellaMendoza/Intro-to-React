bcc305fb1325251ba8f6d900a7403830096770f2
Marcella Mendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-19 06:11:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+// Import your component on the line below and name it Bar
+import Bar from './components/Bar'
+
+
+function App() {
+  return (
+    <main>
+      
+      {/* Show your component using the line below. */}
+      {/* These are comments */}
+      <Bar />
+
+      
+    </main>
+  );
+}
+
+export default App;
